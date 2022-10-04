a0f3e6d43a8660fbb521ac2fe69c9f8399209fc6
AlexandreMakovetsky <alexander.a.makovetsky@gmail.com>
2022-10-04 10:26:15 -0500

    test  Changes to be committed: 	modified:   src/App.js 	new file:   src/reducers.js 	new file:   src/user/Login.js 	new file:   src/user/Logout.js 	new file:   src/user/Register.js 	new file:   src/user/UserBar.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useReducer } from "react";
+import appReducer from "./reducers";
+import UserBar from "./user/UserBar";
 
 function App() {
+  
+  
+  const [state, dispatch] = useReducer(appReducer, {
+    user: "",
+  });
+
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserBar user={state.user} dispatch={dispatch} />
     </div>
   );
 }
